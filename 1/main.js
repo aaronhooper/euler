985f5d720d654258675629b6d777045370c7ecdd
Aaron Hooper <rnjckhpr@gmail.com>
2021-02-25 21:42:21 +0000

    Refactor solution 1

--- 1/main.js
+++ 1/main.js
@@ -1,21 +1,21 @@
-// calculate multiples of 3 under 1000
 let multiplesOfThree = []
+let multiplesOfFive = []
 
+// calculate multiples of 3 under 1000
 for (let i = 3; i < 1000; i += 3) {
     multiplesOfThree.push(i)
 }
 
 // calculate multiples of 5 under 1000
-let multiplesOfFive = []
-
 for (let i = 5; i < 1000; i += 5) {
     multiplesOfFive.push(i)
 }
 
-// merge the two arrays
+// merge the arrays
+// (sets contain no duplicates)
 let mOfThreeAndFive = new Set([...multiplesOfThree, ...multiplesOfFive])
 
-// add them together
+// sum the elements
 const sum = [...mOfThreeAndFive].reduce((all, item) => all + item, 0)
 
 console.log("Sum of all the multiples of 3 or 5 below 1000:", sum)
